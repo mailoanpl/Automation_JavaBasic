7adf8bcafd6aaf861017e4bc073f449fc9f137de
mailoanpl <mailoan1802@gmail.com>
2024-06-05 23:09:22 +0700

    768787

--- src/QuanLySinhVien/Runcode.java
+++ src/QuanLySinhVien/Runcode.java
@@ -3,6 +3,6 @@ package QuanLySinhVien;
 public class Runcode {
 
     public static void main(String[] args) {
-        System.out.println("Hello github");
+        System.out.println("Hello github 6767677");
     }
 }
